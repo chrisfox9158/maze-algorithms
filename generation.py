891f93284340dfe5ae316f3ac1978a25500d3e88
Chris McKeel <chrisfox9158@gmail.com>
2026-07-22 16:20:52 -0400

    Add recursive backtracking-based maze generation system in Generator class
    Generator class holds build() and carve() methods, which generate a blank maze and subsequently populate via Recursive Backtracking. Over multiple iterations, the carve() method finds unvisited neighboring cells and chooses at random; the maze is formed as the system travels to, and opens, new cells.

---
+++ generation.py
@@ -0,0 +1,35 @@
+# Library imports
+import random
+
+# Local imports
+from config import MAZE_SPECS
+
+# Blank maze generation
+class Generator:
+    """Generates a maze via Recursive Backtracking."""
+    def __init__(self):
+        self.width = MAZE_SPECS["width"] if MAZE_SPECS["width"] % 2 != 0 else MAZE_SPECS["width"] + 1
+        self.height = MAZE_SPECS["height"] if MAZE_SPECS["height"] % 2 != 0 else MAZE_SPECS["height"] + 1
+
+    def build(self):
+        """Build a maze populated entirely closed cells."""
+        maze = {(x, y): 1 for x in range(self.width) for y in range(self.height)}
+        return maze
+
+    def carve(self, start_x, start_y, maze):
+        """Given a blank maze and starting positions, uses Recursive Backtracking to build paths."""
+        maze[(start_x, start_y)] = 0
+
+        directions = [(0, -2), (0, 2), (-2, 0), (2, 0)]
+        random.shuffle(directions)
+
+        current_x, current_y = start_x, start_y
+
+        for dx, dy in directions:
+            new_x, new_y = current_x + dx, current_y + dy
+
+            if 0 <= new_x < self.width and 0 <= new_y < self.height and maze[(new_x, new_y)] == 1:
+                maze[(current_x + dx // 2, current_y + dy // 2)] = 0
+                self.carve(new_x, new_y, maze)
+
+        return maze
