4e01241581dc58bcf83ca0f1875f6f9880f06587
Chris McKeel <chrisfox9158@gmail.com>
2026-07-23 03:58:28 -0400

    Add breadth-first search (BFS) algorithm
    Implement a queue-based BFS algorithm inside algorithms/ folder. The search explores all cells at current depth level before continuing to next depth. Beginning at a given x and y, the search pops the oldest path from a deque queue; if the final cell in the given path is the exit, the full path is retured as the solution. Unvisited cells at new depth levels are marked via get_open_neighbors() and get_frontier() methods. Discovered cells are marked as visited and appended to a new path copy; this new path is added to the queue for the next depth layer.

---
+++ algorithms/bfs.py
@@ -0,0 +1,58 @@
+# Library imports
+from collections import deque
+
+# Local imports
+from generation import RecursiveBacktracking
+from config import MAZE_SPECS
+
+# Breadth-first search algorithm
+class BFS:
+    def __init__(self):
+        self.x, self.y = 1, 1
+        self.maze_width = MAZE_SPECS["width"]
+        self.maze_height = MAZE_SPECS["height"]
+        self.prev = {}
+
+    def get_open_neighbors(self, x, y, maze):
+        open_neighbors = []
+        N, S, E, W = (x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)
+        for direction in (N, S, E, W):
+            if maze.get(direction) in (0, 3, 4):
+                open_neighbors.append(direction)
+        return open_neighbors
+
+    def get_frontier(self, visited, x, y, maze):
+        open_neighbors = self.get_open_neighbors(x, y, maze)
+        frontier = []
+        for cell in open_neighbors:
+            if cell not in visited:
+                frontier.append(cell)
+        return frontier
+
+    def run(self, start_x, start_y, maze):
+        visited = set()
+        self.x, self.y = start_x, start_y
+
+        queue = deque([[(self.x, self.y)]])
+        visited.add((self.x, self.y))
+
+        while queue:
+            current_path = queue.popleft()
+            self.x, self.y = current_path[-1]
+
+            if maze[(self.x, self.y)] == 4:
+                return current_path
+            
+            for cell in self.get_frontier(visited, self.x, self.y, maze):
+                visited.add(cell)
+                new_path = list(current_path)
+                new_path.append(cell)
+                queue.append(new_path)
+
+        return None
+
+# Testing
+bfs = BFS()
+generator = RecursiveBacktracking()
+maze = generator.generate_maze()
+print(bfs.run(1, 1, maze))
