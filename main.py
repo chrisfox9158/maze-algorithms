b96e10ce3d4ecda69cb4c5355c014d16f49f5373
Chris McKeel <chrisfox9158@gmail.com>
2026-07-24 13:09:28 -0400

    Add skeleton MultiRun class for multi-episode simulations

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from generation import GENERATORS
 from outputs.visualizer import Visualizer
 
 class SingleRun:
+    """Run a single episode for a given generator and algorithm."""
     def run(self):
         generator_choice = input(f"Generator ({' / '.join(GENERATORS)}): ")
         generator = GENERATORS[generator_choice]()
@@ -18,11 +19,35 @@ class SingleRun:
         results = algorithm.run(maze)
 
         self.visualize(maze, results)
+        return results
 
     def visualize(self, maze, results):
         visualization = Visualizer(maze, results)
         visualization.run()
 
-if int(input(f"Episode count: ")) == 1:
-    instance = SingleRun()
-    instance.run()
+class MultiRun():
+    """Run multiple episodes for a given generator and algorithm."""
+    def run(self, episode_count):
+        generator_choice = input(f"Generator ({' / '.join(GENERATORS)}): ")
+        algorithm_choice = input(f"Algorithm ({' / '.join(ALGORITHMS)}): ")
+        results = []
+
+        for episode in range(episode_count):
+            generator = GENERATORS[generator_choice]()
+            maze = generator.generate_maze()
+
+            algorithm = ALGORITHMS[algorithm_choice]()
+            results.append(algorithm.run(maze))
+
+        return results
+
+if __name__ == "__main__":
+    episodes = int(input(f"Episode count: "))
+    if episodes == 1:
+        instance = SingleRun()
+        instance.run()
+        print("Simulation complete!")
+    else:
+        instance = MultiRun()
+        instance.run(episodes)
+        print("Simulation complete!")
